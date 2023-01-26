3fe9844cbd7da135b81030f1d85c04582c09612e
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2023-01-26 15:10:54 -0800

    cleanup, base iterator setup

--- Microbiome Proj.py
+++ Microbiome Proj.py
@@ -3,29 +3,22 @@
 
 # Take the results.tsv file and parse it
 
-# In[10]:
-
-
 import pandas as pd
 import numpy as np
 
-hash_map = {}
-
-
-# In[1]:
-
-
+# construct df with all data from results.tsv
 df = pd.read_table('results.tsv')
-#print(df)
-
+# print columns of df for dev use in constructing filtered_df later
+print('---------')
 print(df.columns)
-#filtered_df = df[['#Query_id, 'e-value', 'Query_Coverage', 'Hit_Coverage', 'Query_Pfam']]
-filtered_df = df[['#Query_id', 'Query_Coverage', 'e-value', 'Hit_Coverage', 'Query_Pfam']]
-print(filtered_df)
+print('---------')
 
+# construct filtered_df with only relevant data points for each protein matche
+filtered_df = df[['#Query_id', '%_identity', 'e-value', 'Q_start', 'Q_end', 'S_start', 'S_end', 
+'Query_Coverage', 'Hit_Coverage', 'Query_Pfam']]
+#print(filtered_df)
 
-# In[ ]:
-
-
+for index, row in filtered_df.iterrows():
+    print(row['#Query_id'])
 
 
