67b217d0ad35a2ee9eea21313971721950fc0847
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2023-01-27 18:07:22 -0800

    Update Microbiome_proj.py

--- Microbiome_proj.py
+++ Microbiome_proj.py
@@ -13,6 +13,7 @@ print('---------')
 print(df.columns)
 print('---------')
 
+# create empty dfs for tagging with green, yellow, and red labels
 green_df = df.copy()
 green_df = green_df.iloc[0:0]
 yellow_df = green_df.copy()
@@ -23,7 +24,18 @@ filtered_df = df[['#Query_id', '%_identity', 'e-value', 'Q_start', 'Q_end', 'S_s
 'Query_Coverage', 'Hit_Coverage', 'Query_Pfam']]
 #print(filtered_df)
 
+#Example content:
+#  1.A.1.1.1 =>  ["1.A.1.1.1-P0A334"],
+#  3.A.1.1.1 =>  ["3.A.1.1.1-P02916", ""3.A.1.1.1-XXXXX", "3.A.1.1.1-YYYYYY", "3.A.1.1.1-ZZZZZZZZZ"],
+#  ....
+tcdbSystems = {}
+
 # TODO
+
+#This function will fill the dictionary tcdbSystems with data.
+def parseTCDBcontent():
+     return True
+
 def isSingleComp(row):
     return True
 
@@ -36,7 +48,7 @@ def hCoverage(row):
 def eVal(row):
     return 0
 
-def pfamDoms(row):
+def PfamDoms(row):
     common_doms = ""
     return common_doms
 
@@ -45,7 +57,7 @@ for index, row in df.iterrows():
     if(isSingleComp(row)):
         if(eVal(row) <= -10 and pfamDoms().size != 0):
             green_df = green_df.append([row])
-        elif(eVal(row) <= -5 and qCoverage() >= 90 and hCoverage >= 90):
+        elif(eVal(row) < -3 and qCoverage() >= 90 and hCoverage >= 90):
             yellow_df = yellow_df.append([row])
         else:
             red_df = red_df.append([row])
