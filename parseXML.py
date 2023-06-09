b019b3b4c305c5eca4bbf85da975bd037e3fae11
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2023-06-09 13:45:08 -0700

    Working on parse, added sample dir

---
+++ parseXML.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+import xml
+
+# GCF_009648975.1/xml/WP_064323585.1.xml
+xmlfile = "GCF_009648975.1/xml/WP_064323585.1.xml"
+
+tree = ET.parse(xmlfile)
+root = tree.getroot()
+
+print(root)
+
+for item in root.findall('Hit'):
+    print(item)
+    print(item.find('Hit_def'))
